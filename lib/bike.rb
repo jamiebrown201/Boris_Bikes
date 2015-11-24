001ac9f4dd56fbd6844602eda1e3fc3fe62c6f0e
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-24 18:03:03 +0000

    Added docking feature and docked return feature

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,16 +1,4 @@
 class Bike
-	attr_reader :name
-
-	def initialize
-		@working = true
-		@name = name
-		$docked = false
-	end
-
-	def docking
-		$docked = true
-	end
-
   def working?
     true
   end
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,13 @@
 class DockingStation
+
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
-  def docked?
-    if $docked == true
-      "#{Bike} is docked"
-    else
-      "#{Bike} is not docked"
-    end
+  def dock(bike)
+   @bike = bike
   end
 
 end
